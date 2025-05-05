1517016b2bc23ebed2236e855d8841f9937a3969
DESKTOP-1VV4LDO\Pedro <peusodati@gmail.com>
2025-05-05 19:40:41 -0300

    chore: adiciona ESLint e Prettier ao projeto

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import axios from 'axios'
 import cors from 'cors'
 import express from 'express'
+import z from 'zod'
 
 const app = express()
 
@@ -7,11 +9,31 @@ app.use(express.json())
 
 app.use(cors({}))
 
-app.get('/', (request, response) => {
-    response.send('Ok.')
+app.post('/perguntar', async (request, response) => {
+  try {
+    const bodyParams = z.object({
+      texto: z.string(),
+    })
+
+    const { texto } = bodyParams.parse(request.body)
+
+    const responseOllama = await axios.post('http://localhost:11434/api/generate', {
+      model: 'mistral',
+      prompt: texto,
+      stream: false,
+    })
+
+    return response.status(200).json({
+      resposta: responseOllama.data.response,
+    })
+  } catch (error) {
+    return response.status(500).json({
+      error,
+      message: 'Erro interno',
+    })
+  }
 })
 
 app.listen(3333, () => {
-    console.log('App on!')
+  console.log('App on!')
 })
-
